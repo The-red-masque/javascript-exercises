6fb761f81291f96d0956f5faf0d70ab8f9dc3d1d
the-telltale-heart <chris.farlee@gmail.com>
2026-04-20 20:03:16 -0700

    Got string to repeat & tested the first two tests.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+if (num < 0) {
+    return 'ERROR';
+} else {
+    // For str in strs:
+    //     string=string+string
+    //     return string
+    let result='';
+    for (let i = 0; i < num; i++) {
+      result += string;
+}
+return result;
+}
 };
 
 // Do not edit below this line
