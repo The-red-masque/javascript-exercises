59f831d2c254ffccd84ae93517e069c337993d85
the-telltale-heart <chris.farlee@gmail.com>
2026-04-21 13:23:32 -0700

    reverseString successfully reverse strings.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+function reverseString (string) {
+    let reverseString='';
+    for (let i = string.length - 1; i >= 0; i--) {
+    reverseString += string[i];
+    
+}; return reverseString;
 
 };
 
