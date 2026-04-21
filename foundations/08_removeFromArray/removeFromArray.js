98b99c18cf691af3e525717bf7a3ac2fc6952bc9
the-telltale-heart <chris.farlee@gmail.com>
2026-04-21 15:35:48 -0700

    Solved Exercise 8 with solutions from odin. I ended up getting caught between two different possible solutions.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
 
+// Alternative Version less effecient version.
+// function removeFromArray (array, ...args) {
+  // const newArray = [];
+  // For each != args push to new array
+  // array.forEach((item) => {
+    // if (!args.includes(item)) {
+      // newArray.push(item);
+    // }
+// });
+    // return newArray;
+// };
+// Look up ""...args" in functions.
+// Look up "!args"
+
+
+const removeFromArray = function(array, ...args) {
+  return array.filter(val => !args.includes(val))
+}
 // Do not edit below this line
 module.exports = removeFromArray;
