5d56c5fde63619eaa0897c3188b21cd4e1935ae9
the-telltale-heart <chris.farlee@gmail.com>
2026-04-22 17:54:58 -0700

    Finished the leapYear function. It took much more time than I expected.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+if ( year % 4 === 0) { 
+    if ( year < 100) {return true;}  // handles leap years that won't be covered by the other truth conditions. 
+    else if ( year % 400 === 0) {return true;}
+    else if ( year % 100 !== 0) {return true;}
+    else {return false;}
+    } else {return false;}
+}
 
 // Do not edit below this line
 module.exports = leapYears;
