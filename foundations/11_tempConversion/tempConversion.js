f71b5f0d0c9ef5e1d9b263884ecd678204b5baed
the-telltale-heart <chris.farlee@gmail.com>
2026-06-10 23:08:52 -0700

    Completed calculator in JavaScript.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+// Both functions should be rounded to one decimal point. Ex 37.8 instead of 37.77777778
+const convertToCelsius = function(fahrenheit) {
+    // 32F - 32 * 5/9
+    const celsius = (fahrenheit - 32) * (5/9);
+    // Celsius rounded.
+    const celsiusRounded = rounded = Math.round(celsius * 10) / 10
+    return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    // F = 
+    const fahrenheit = (celsius * (9/5)) + 32;
+    // fahrenheitRounded 
+    const fahrenheitRounded = Math.round(fahrenheit * 10) / 10
+    return fahrenheitRounded;
 };
 
 // Do not edit below this line
