0bc39b7dfafe684fc3a9bfb797fe04394c0c4133
the-telltale-heart <chris.farlee@gmail.com>
2026-06-16 13:40:38 -0700

    Cleaned up for readibility.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -7,13 +7,11 @@ const fibonacci = function(sequencePlace) {
     let number2 = 1;
     let sum;
 
-    // const isZero = (number === 0);
-    // const isOne = (number === 1);
-    // let i = 2;
+    const isZero = (number === 0);
+    const isOne = (number === 1);
 
-
-    if (number === 0) {return number1;}
-    else if (number === 1) {return number2;}
+    if (isZero) {return number1;}
+    else if (isOne) {return number2;}
     else {
       for (let i = 2; i<= number; i++) {
         sum = number1 + number2;
@@ -24,20 +22,6 @@ const fibonacci = function(sequencePlace) {
     }
   } else {return "OOPS";}
 }
-/*if (isPositive) {
-    if (fibonacciNumber == 0) {let number = 0;return number;}
-    else if (fibonacciNumber == 1) {let number =1;return number;}
-    // if(fibonacciNumber == )
-  else if (fibonacciNumber == 4) {
-    let number = 3;
-    return number;
-  } else if (fibonacciNumber == 6) {
-  let number = 8;
-  return number;
-} else if (fibonacciNumber == 10) {let number = 55;return number;}
-else if (fibonacciNumber == 15) {let number = 610;return number;}
-else if (fibonacciNumber == 25) {let number = 75025;return number;}
-else {}} else {number = "OOPS";return number;};}*/
 
 
 // Do not edit below this line
