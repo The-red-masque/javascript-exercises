da1d26f7cf11204867f77a66dcaa5e01f2002ed4
the-telltale-heart <chris.farlee@gmail.com>
2026-06-17 17:31:15 -0700

    Functioning when both yearOfDeath and yearOfBirth are provided for object.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  
+  /*
+  Sort through Array to find the oldest person
+  - Add age to object?
+  - sort array by age
+  - If there is no death, create a yearOfDeath based on current year.
+  */
+  
+  //const hasDied = item.includes("yearOfDeath");
+  //const thisYear = new Date().getFullYear();
+    function sortByAge(arr) {
+    arr.sort((a, b) => b.age - a.age);
+    }
+  people.forEach((item) => {
+    item.age = item.yearOfDeath - item.yearOfBirth;
+  });
+
+  sortByAge(people);
+  let oldestPersonObject = people.at(0);
+  console.log(oldestPersonObject);
+  //let oldestPersonName = oldestPersonObject.name; 
+  //console.log(oldestPersonName);
+  return oldestPersonObject;
+  
 
 };
 
